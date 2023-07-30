c4008222546e58bdc25dc3c92d3c2fd102a5b109
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-07-30 16:15:15 -0300

    reset my server.js and taking another crack at this

--- server.js
+++ server.js
@@ -1,35 +1,15 @@
 const express = require('express');
 const path = require('path');
+const PORT = 3001; 
 const app = express();
-const port = 3001;
 
-// GET route to get all of the terms
-app.get('/api/notes', (req, res) => res.json(notesData));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// GET route that returns any specific term
-app.get('/api/notes/:note', (req, res) => {
-  // Coerce the specific search term to lowercase
-  const requestednote = req.params.note.toLowerCase();
-
-  // Iterate through the terms name to check if it matches `req.params.term`
-  for (let i = 0; i < termData.length; i++) {
-    if (requestedTerm === termData[i].term.toLowerCase()) {
-      return res.json(termData[i]);
-    }
-  }
-
-  // Return a message if the term doesn't exist in our DB
-  return res.json('No match found');
-});
-
-// Fallback route for when a user attempts to visit routes that don't exist
-app.get('*', (req, res) =>
-  res.send(
-    `Make a GET request using Insomnia to <a href="http://localhost:${PORT}/api/terms">http://localhost:${PORT}/api/terms</a>`
-  )
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// Listen for connections
 app.listen(PORT, () =>
-  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
