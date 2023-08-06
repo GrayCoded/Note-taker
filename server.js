91406adb5682d3a1cfc0490879938c734692a3a3
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-08-06 10:33:15 -0300

    added app.use to the server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 const PORT = 3001;
 const app = express();
 const api = require('./routes/index.js');
@@ -26,6 +25,8 @@ app.get('/api/notes', (req, res) => {
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`Note Taker listening at http://localhost:${PORT} 🚀`)
